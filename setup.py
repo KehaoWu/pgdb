27a80b8b6c25285c37545ca67298337b97487e61
Kehao Wu <wukehao@memect.co>
2017-04-06 09:54:41 +0800

    [feature]update from 0.0.4 to 0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pgdb',
-      version='0.0.4',
+      version='0.0.5',
       description="PostgreSQL wrapper",
       long_description="",
       classifiers=["Development Status :: 5 - Production/Stable",
